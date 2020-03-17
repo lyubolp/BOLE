9addde2468d5e5f5f4ee312185f8bfa188b74e1f
Luchev <luchevz@gmail.com>
2020-03-17 22:27:21 +0200

    Feat main Modules
    Add module skeletons for
    cloud, courses, dashboard, grades, home, homeworks, social, user

---
+++ bole/src/app/grades/grades.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GradesRoutingModule } from './grades-routing.module';
+import { GradesComponent } from './grades/grades.component';
+
+
+@NgModule({
+  declarations: [GradesComponent],
+  imports: [
+    CommonModule,
+    GradesRoutingModule
+  ]
+})
+export class GradesModule { }
