25b87c87f11547c966f4b65ec8180cb6b072e346
Luchev <luchevz@gmail.com>
2020-03-18 21:08:28 +0200

    Add UI for Courses Module

--- bole/src/app/courses/courses.module.ts
+++ bole/src/app/courses/courses.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 
 @NgModule({
-  declarations: [CoursesComponent],
+  declarations: [CoursesComponent, CourseCardComponent],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    FormsModule,
   ]
 })
 export class CoursesModule { }
--- bole/src/app/courses/courses/courses.component.ts
+++ bole/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseCard } from '../course-card';
 
 @Component({
   selector: 'app-courses',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-
   constructor() { }
 
+  courseCardsFiltered: CourseCard[] = [];
+  courseCards: CourseCard[] = [];
+  searchText = '';
+
   ngOnInit(): void {
+    this.courseCards.push({ id: 1, name: 'Data structures and algorithms' });
+    this.courseCards.push({ id: 2, name: 'Object-oriented programming' });
+    this.courseCards.push({ id: 3, name: 'Introduction to programming' });
+    this.courseCards.push({ id: 4, name: 'Introduction to Software Engineering' });
+    this.courseCards.push({ id: 5, name: 'Quality assurance' });
+    this.courseCards.push({ id: 6, name: 'Discrete mathematics' });
+    this.courseCards.push({ id: 7, name: 'C++ for beginners' });
+    this.courseCards.push({ id: 8, name: 'Intermediate C++' });
+
+    this.filterCards();
   }
 
+  filterCards() {
+    console.log(this.searchText);
+    if (this.searchText === '') {
+      this.courseCardsFiltered = this.courseCards;
+    } else {
+      this.courseCardsFiltered = this.courseCards.filter((card) => card.name.toLowerCase().includes(this.searchText.toLowerCase()));
+    }
+  }
 }
