0061f79c8fcdae06dfecda74ac8f9bca751b3d1d
Ivaylo Ivanov <ivaylogi98@gmail.com>
2020-04-22 01:53:01 +0300

    Revert "Worked on social component"
    This reverts commit d34711a4bdb6aa509dafa3cb99de91b2bdcee3b6.

--- bole/src/app/social/social.module.ts
+++ bole/src/app/social/social.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { SocialRoutingModule } from './social-routing.module';
 import { SocialComponent } from './social/social.component';
-import { SocialCardComponent } from './social-card/social-card.component';
 
 
 @NgModule({
-  declarations: [SocialComponent, SocialCardComponent],
+  declarations: [SocialComponent],
   imports: [
     CommonModule,
-    SocialRoutingModule,
-    FormsModule
+    SocialRoutingModule
   ]
 })
 export class SocialModule { }
--- bole/src/app/social/social/social.component.ts
+++ bole/src/app/social/social/social.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SocialCard } from '../social-card';
 
 @Component({
   selector: 'app-social',
@@ -10,31 +9,7 @@ export class SocialComponent implements OnInit {
 
   constructor() { }
 
-  socialGeneralCardsFiltered: SocialCard[] = [];
-  socialPersonalCardsFiltered: SocialCard[] = [];
-  SocialGeneralCards: SocialCard[] = [];
-  SocialPersonalCards: SocialCard[] = [];
-  searchText = '';
-
   ngOnInit(): void {
-    this.SocialGeneralCards.push({ id: 1, name: 'Софтуерно инженерство 2017-2021', participants: 121, lastActivePost: 'Домашно по АСИ ???' });
-    this.SocialGeneralCards.push({ id: 2, name: 'Софтуерно инженерство 2017-2012, група 2', participants: 22, lastActivePost: 'Домашно по АСИ ???' });
-    this.SocialGeneralCards.push({ id: 3, name: 'ФМИ', participants: 500, lastActivePost: 'КСК?' });
-    this.SocialPersonalCards.push({ id: 4, name: 'Обектно-ориентирано програмиране', participants: 121, lastActivePost: 'Домашно по АСИ ???' });
-    this.SocialPersonalCards.push({ id: 5, name: 'Анализ на софтуерните изисвания', participants: 121, lastActivePost: 'Домашно по АСИ ???' });
-    this.SocialPersonalCards.push({ id: 6, name: 'Web технорлогии', participants: 121, lastActivePost: 'Домашно по АСИ ???' });
-
-    this.filterCards();
   }
 
-  filterCards() {
-    console.log(this.searchText);
-    if (this.searchText === '') {
-      this.socialGeneralCardsFiltered = this.SocialGeneralCards;
-      this.socialPersonalCardsFiltered = this.SocialPersonalCards;
-    } else {
-      this.socialGeneralCardsFiltered = this.SocialGeneralCards.filter((generalCard) => generalCard.name.toLowerCase().includes(this.searchText.toLowerCase()));
-      this.socialPersonalCardsFiltered = this.socialPersonalCardsFiltered.filter((personalCard) => personalCard.name.toLowerCase().includes(this.searchText.toLowerCase()));
-    }
-  }
 }
