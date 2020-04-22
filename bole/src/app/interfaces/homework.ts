46d8e6d62733f77d974e5bf894657fc29048a34c
luchev <luchevz@gmail.com>
2020-04-23 00:22:30 +0300

    Fix interfaces/homework.ts to have a name

--- bole/src/app/interfaces/homework.ts
+++ bole/src/app/interfaces/homework.ts
@@ -1,6 +1,6 @@
-/*
-  https://xd.adobe.com/view/d223df1e-6c52-48ce-6594-d5fb4215f572-a412/screen/df70a561-9c6e-4c22-b623-86638e4b2240/Homework
-  Extends HomeworkCard info adding the id of the discussion, how many times we can attempt the homework and the grade
+/**
+ * https://xd.adobe.com/view/d223df1e-6c52-48ce-6594-d5fb4215f572-a412/screen/df70a561-9c6e-4c22-b623-86638e4b2240/Homework
+ * Extends HomeworkCard info adding the id of the discussion, how many times we can attempt the homework and the grade
  */
 
 
@@ -11,4 +11,5 @@ export interface Homework extends HomeworkCard {
     discussionId: number;
     attemptsLeft: number;
     grade: Grade;
+    name: string;
 }
