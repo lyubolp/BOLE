91937a4bbda904c5e5c8217cb3a040339a4a6bac
luchev <luchevz@gmail.com>
2020-04-23 00:24:06 +0300

    Fix homework component to use the homework service

--- bole/src/app/single-homework/single-homework/single-homework.component.ts
+++ bole/src/app/single-homework/single-homework/single-homework.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Homework } from '../../interfaces/homework';
 import {createUrlResolverWithoutPackagePrefix} from '@angular/compiler';
+import { HomeworkService } from 'src/app/services/homework.service';
 @Component({
   selector: 'app-single-homework',
   templateUrl: './single-homework.component.html',
@@ -8,11 +9,13 @@ import {createUrlResolverWithoutPackagePrefix} from '@angular/compiler';
 })
 export class SingleHomeworkComponent implements OnInit {
 
-  courseName = 'Обектно-ориентирано програмиране - домашно 1';
-  taskId = 1;
-  attemptsLeft = 0;
-  grade = 0;
-  constructor() {
+  homework: Homework;
+
+  // courseName = 'Обектно-ориентирано програмиране - домашно 1';
+  // taskId = 1;
+  // attemptsLeft = 0;
+  // grade = 0;
+  constructor(private homeworkService: HomeworkService) {
     // pass `current` as an argument to the constructor or whatever
     /*current: Homework = {};
     this.taskId = current.id;
@@ -21,7 +24,11 @@ export class SingleHomeworkComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const doc: HTMLElement = document.getElementById('test');
-    doc.setAttribute('src', '../../../assets/mockup_assets/hw-' + this.taskId.toString() + '.pdf');
+    // Get homework by id
+    this.homeworkService.getHomework(1).subscribe((result) => {
+      this.homework = result;
+      const doc: HTMLElement = document.getElementById('test');
+      doc.setAttribute('src', '../../../assets/mockup_assets/hw-' + this.homework.grade.taskId.toString() + '.pdf');
+    });
   }
 }
