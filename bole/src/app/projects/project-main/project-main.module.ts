2e0b7963257df99cf5703b8f857d1a7d307bd760
Lyuboslav Karev <lyubo.karev@gmail.com>
2020-05-02 00:03:02 +0300

    Added modules for the project page and code-editor & added routing for it

---
+++ bole/src/app/projects/project-main/project-main.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectMainComponent } from './project-main/project-main.component';
+
+
+
+@NgModule({
+  declarations: [ProjectMainComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProjectMainModule { }
