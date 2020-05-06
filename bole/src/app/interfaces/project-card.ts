7fcf97989d526341519042076a12007edd2da8ca
Lyuboslav Karev <lyubo.karev@gmail.com>
2020-05-06 22:46:01 +0300

    Added project main page and project card

---
+++ bole/src/app/interfaces/project-card.ts
@@ -0,0 +1,9 @@
+export interface ProjectCard {
+    id: number;
+    name: string;
+    description: string;
+    dueDate: Date;
+    amountOfMembers: number;
+    newMessages: number;
+    colorCode: string;
+}
