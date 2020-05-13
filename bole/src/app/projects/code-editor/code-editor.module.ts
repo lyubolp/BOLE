35036e7ac960b5e356af85ce0130f8edb3df1db7
luchev <luchevz@gmail.com>
2020-05-13 12:30:54 +0300

    Fix code editor dependencies
    Monaco is now working - check it out under http://localhost:4200/projects/code-editor

--- bole/src/app/projects/code-editor/code-editor.module.ts
+++ bole/src/app/projects/code-editor/code-editor.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeEditorComponent } from './code-editor/code-editor.component';
-
+import { MonacoEditorModule } from 'ngx-monaco-editor';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [CodeEditorComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MonacoEditorModule,
+    FormsModule
   ]
 })
-export class CodeEditorModule { }
+export class CodeEditorModule {
+
+}
