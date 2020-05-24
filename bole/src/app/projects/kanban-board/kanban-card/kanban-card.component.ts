5c07e952dd1465b5fae5cd10079069e56f78b6b6
Lyuboslav Karev <lyubo.karev@gmail.com>
2020-05-24 18:58:24 +0300

    Added the Kanban board

--- bole/src/app/projects/kanban-board/kanban-card/kanban-card.component.ts
+++ bole/src/app/projects/kanban-board/kanban-card/kanban-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Card, Priority} from '../../../interfaces/kanban/card';
 
 @Component({
@@ -10,24 +10,39 @@ export class KanbanCardComponent implements OnInit {
   @Input() cardData: Card;
   name = '';
   description = '';
-  priority: Priority;
-  assignees: number[] = [];
+  priority = '';
+  assignees: string[] = [];
   dueDate: Date;
   timeLeft = '';
 
   cardDOMObject: HTMLElement;
+  descriptionDomObject: HTMLElement;
   constructor() { }
 
   ngOnInit(): void {
     this.name = this.cardData.name;
     this.description = this.cardData.description;
-    this.priority = this.cardData.priority;
-    this.assignees = this.cardData.assignees;
-    this.dueDate = this.cardData.dueDate;
-    console.log(this.dueDate);
-    this.timeLeft = this.convertTimeToString(this.dueDate.getTime() - new Date().getTime());
 
-    this.cardDOMObject = document.getElementById('card-main');
+    if (this.cardData.priority === Priority.Low) {
+      this.priority = 'нисък';
+    } else if (this.cardData.priority === Priority.Medium) {
+      this.priority = 'среден';
+    } else {
+      this.priority = 'висок';
+    }
+    for (const assignee of this.cardData.assignees) {
+      if (assignee === 1) {
+        this.assignees.push('lyubo');
+      } else if (assignee === 2) {
+        this.assignees.push('luchev');
+      } else if (assignee === 3) {
+        this.assignees.push('zhechko');
+      } else {
+        this.assignees.push('ivo');
+      }
+    }
+    this.dueDate = this.cardData.dueDate;
+    this.timeLeft = this.convertTimeToString(this.dueDate.getTime() - new Date().getTime());
   }
 
   convertTimeToString(time: number): string {
@@ -45,11 +60,49 @@ export class KanbanCardComponent implements OnInit {
     return result;
   }
 
-  openCard(): void {
-    this.cardDOMObject.style.position = 'absolute';
-    this.cardDOMObject.style.left = '0px';
-    this.cardDOMObject.style.top = '0px';
-    this.cardDOMObject.style.width = '50%';
-    this.cardDOMObject.style.height = '50%';
+  openCard(e): void {
+    if (e.target.id === 'close' || e.target.id === 'id') {
+      return;
+    }
+    console.log(e.target.id);
+    console.log("Called");
+    document.getElementById('card-main').classList.add('card-main-opened');
+    document.getElementById('time-left-icon').style.display = 'none';
+    document.getElementById('priority-buttons').style.display = 'block';
+    document.getElementById('description-box').style.display = 'block';
+    document.getElementById('darken-page').style.display = 'block';
+    document.getElementById('time-left-container').classList.add('opened-card');
+    document.getElementById('time-left-text').classList.remove('hide-element');
+    document.getElementById('time-left-text').classList.add('sub-panel-title');
+    document.getElementById('new-due-date').style.display = 'inline';
+    document.getElementById('team').style.display = 'block';
+    document.getElementById('team').classList.add('team-panel-open');
+    document.getElementById('card-title').classList.remove('panel-title');
+    document.getElementById('card-title').classList.add('page-title');
+    document.getElementById('save-exit-buttons').style.display = 'flex';
+    document.getElementById('save-exit-buttons').style.flexDirection = 'row-reverse';
+
+
+    // this.descriptionDomObject.style.visibility = 'visible';
+    // document.getElementById('priority-text').style.visibility = 'visible';
+  }
+
+  closeCard(): void {
+    console.log(document.getElementById('card-main').classList);
+    document.getElementById('card-main').classList.remove('card-main-opened');
+    console.log(document.getElementById('card-main').classList);
+    document.getElementById('time-left-icon').style.display = 'block';
+    document.getElementById('priority-buttons').style.display = 'none';
+    document.getElementById('description-box').style.display = 'none';
+    document.getElementById('darken-page').style.display = 'none';
+    document.getElementById('time-left-container').classList.remove('opened-card');
+    document.getElementById('time-left-text').classList.add('hide-element');
+    document.getElementById('time-left-text').classList.remove('sub-panel-title');
+    document.getElementById('new-due-date').style.display = 'none';
+    document.getElementById('team').style.display = 'none';
+    document.getElementById('team').classList.remove('team-panel-open');
+    document.getElementById('card-title').classList.add('panel-title');
+    document.getElementById('card-title').classList.remove('page-title');
+    document.getElementById('save-exit-buttons').style.display = 'none';
   }
 }
