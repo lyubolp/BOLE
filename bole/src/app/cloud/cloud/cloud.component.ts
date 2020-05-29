f1cd3700e998c4f3e2787e0e1f8b47cee846aeb8
Zhechko Popov <zhspopov@gmail.com>
2020-05-29 22:43:36 +0300

    added new folder button

--- bole/src/app/cloud/cloud/cloud.component.ts
+++ bole/src/app/cloud/cloud/cloud.component.ts
@@ -84,7 +84,7 @@ export class CloudComponent implements OnInit {
     path_tag2.setAttribute("fill", "none");
 
     var h6_tag = document.createElement("h6");
-    h6_tag.textContent = selectedFileName;
+    h6_tag.textContent = "new folder";
     h6_tag.setAttribute("style", "align-self:center;max-width:70px;text-overflow:ellipsis;overflow:hidden;")
 
     svg_tag.appendChild(path_tag1);
@@ -92,6 +92,56 @@ export class CloudComponent implements OnInit {
 
     div_tag.appendChild(svg_tag);
     div_tag.appendChild(h6_tag);
+
+    panel.appendChild(div_tag);
+  }
+
+  newFolder() {
+    var panel = document.getElementById("file-item-panel");
+
+    var div_tag = document.createElement("div");
+    div_tag.className = "file-item";
+    div_tag.setAttribute("style", "margin:0.5%;align-items:center;text-align:center;width:70px;height: 90px;");
+    
+    var svg_tag = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg_tag.setAttribute("width", "60");
+    svg_tag.setAttribute("height", "60");
+    svg_tag.setAttribute("viewBox", "0 0 24 24");
+
+    var path_tag1 = document.createElementNS('http://www.w3.org/2000/svg', "path");
+    path_tag1.setAttribute("d", "M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z");
+    path_tag1.setAttribute("fill", "#5F14B4");
+
+    var path_tag2 = document.createElementNS('http://www.w3.org/2000/svg',"path");
+    path_tag2.setAttribute("d", "M0 0h24v24H0z");
+    path_tag2.setAttribute("fill", "none");
+
+    var input_tag = document.createElement("input");
+    input_tag.type = "text";
+    input_tag.setAttribute("style", "border:none;background:transparent;outline:none;color:white;font-family:0.6em;padding:none;text-align:center;width:70px;margin:0,5%;");
+    input_tag.value = "new_folder"; 
+    var value;
+    input_tag.addEventListener("keypress", function (event){
+      if(event.key === 'Enter') {
+        value = input_tag.value;
+        div_tag.removeChild(input_tag);
+        var h6_tag = document.createElement("h6");
+        h6_tag.textContent = input_tag.value;
+        h6_tag.setAttribute("style", "align-self:center;max-width:70px;text-overflow:ellipsis;overflow:hidden;")
+        div_tag.appendChild(h6_tag);
+      }  
+    })
+    
+
+    var h6_tag = document.createElement("h6");
+    h6_tag.textContent = input_tag.textContent;
+    h6_tag.setAttribute("style", "align-self:center;max-width:70px;text-overflow:ellipsis;overflow:hidden;")
+
+    svg_tag.appendChild(path_tag1);
+    svg_tag.appendChild(path_tag2);      
+
+    div_tag.appendChild(svg_tag);
+    div_tag.appendChild(input_tag);
 
     panel.appendChild(div_tag);
   }
